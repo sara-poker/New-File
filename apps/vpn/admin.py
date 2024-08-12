353cb24c9f7e16d8cec6d1ae11c1ff5801ec0167
sara-poker <spoker1412@gmail.com>
2024-08-12 11:54:24 +0330

    add country and Isp to admin panel

--- apps/vpn/admin.py
+++ apps/vpn/admin.py
@@ -2,15 +2,27 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Vpn ,Test
+from .models import Vpn, Test, Country, Isp
+
 
 # Register your models here.
 class VpnAdmin(admin.ModelAdmin):
     list_per_page = 30
 
+
 class TestAdmin(admin.ModelAdmin):
     list_per_page = 30
 
-admin.site.register(Vpn,VpnAdmin)
-admin.site.register(Test,TestAdmin)
 
+class CountryAdmin(admin.ModelAdmin):
+    list_per_page = 30
+
+
+class IspAdmin(admin.ModelAdmin):
+    list_per_page = 30
+
+
+admin.site.register(Vpn, VpnAdmin)
+admin.site.register(Test, TestAdmin)
+admin.site.register(Country, CountryAdmin)
+admin.site.register(Isp, IspAdmin)
